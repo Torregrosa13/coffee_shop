8e7aa5a1941c10e36b10162bcf7daebc8a47c6d2
Torregrosa13 <garciatorregrosacarlos@gmail.com>
2024-11-26 18:17:21 +0100

    git setup

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProductFormView
+from .views import ProductListView
 
 urlpatterns = [
     path('agregar/', ProductFormView.as_view(), name="add_product"),
+    path('', ProductListView.as_view(), name='list_product')
 ]
